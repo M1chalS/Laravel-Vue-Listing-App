2de8cccd4370a55c78b72bb0cb6129a970e1ea83
M1chalS <michalszajner@wp.pl>
2023-03-30 19:31:06 +0200

    First Vue app

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -7,10 +7,12 @@ use Illuminate\Http\Request;
 class IndexController extends Controller
 {
     public function index() {
-        return "Index";
+        return inertia('Index/Index', [
+            'message' => "Hello from Laravel"
+        ]);
     }
 
     public function show() {
-        return "Show";
+        return inertia('Index/Show');
     }
 }
